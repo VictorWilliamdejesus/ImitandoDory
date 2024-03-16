88fa65fdab3e59f46f0f91aec947a75dc1c2c8ea
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 22:12:17 -0300

    finaliza dori

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Console.WriteLine("Victor".ToUpper().Replace("V", "@"));
+
+Console.Clear();
+Console.WriteLine("--- Tradutor de Baleês---\n");
+Console.Write("Digite o texto a ser traduzido: ");
+string texto = Console.ReadLine()!;
+Console.WriteLine("\nA Dori diria...\n");
+Console.WriteLine(texto.Replace("a", "aaa")
+.Replace("e", "eee")
+.Replace("i", "iii")
+.Replace("o", "ooo")
+.Replace("u", "uuu")
+);
+Console.WriteLine();
